339fe7fc85d7bd5c1db55723352f3d9911bd163c
Sean Hayes <sean.owen.ext@allianz-trade.com>
2025-11-13 11:01:00 +0100

    Initial Commit

---
+++ HayescapeNavigator/HayescapeNavigatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HayescapeNavigatorApp.swift
+//  HayescapeNavigator
+//
+//  Created by Sean Hayes on 13/11/25.
+//
+
+import SwiftUI
+
+@main
+struct HayescapeNavigatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
