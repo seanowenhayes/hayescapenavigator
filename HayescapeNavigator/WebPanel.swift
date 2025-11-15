110e1de4e0ad1fc42798817c1dde98d60b769b55
sean <seanowenhayes@gmail.com>
2025-11-15 09:13:08 +0100

    Add basic keyboard shortcuts

--- HayescapeNavigator/WebPanel.swift
+++ HayescapeNavigator/WebPanel.swift
@@ -1,24 +1,32 @@
-//
-//  WebPanel.swift
-//  HayescapeNavigator
-//
-//  Created by Sean Hayes on 13/11/25.
-//
-
 import SwiftUI
 import WebKit
 
 struct WebPanel: View {
     @State private var page = WebPage()
+    @State private var showTabSelection: Bool = false
 
     
         var body: some View {
             VStack {
-                Text(page.title)
+                if showTabSelection {
+                    Text(page.title)
+                }
                 WebView(page)
             }
             .onAppear {
                 page.load(URLRequest(url: URL(string: "https://www.swift.org")!))
+            }.onKeyPress { event in
+                if event.modifiers.contains(.command) {
+                  if event.characters.contains("t") {
+                      showTabSelection.toggle()
+                      return .handled
+                  }
+                    if event.characters.contains("r") {
+                        page.reload()
+                        return .handled
+                    }
+                }
+                return .ignored
             }
         }
 }
